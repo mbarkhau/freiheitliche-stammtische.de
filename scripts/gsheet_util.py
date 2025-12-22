fd1e01720f8c09444f4192eb999d5bec98492441
Manuel Barkhau <mbarkhau@gmail.com>
2025-12-22 22:12:41 +0100

    wip

--- scripts/gsheet_util.py
+++ scripts/gsheet_util.py
@@ -69,56 +69,56 @@ _last_lookup = 0.0
 VERSION = "plz_heatmap_prototype-v1.0"
 
 CITIES = [
-    { 'population': 3755000, 'coords': [52.5200, 13.4050], 'name': 'Berlin' },
-    { 'population': 1862565, 'coords': [53.5511,  9.9937], 'name': 'Hamburg' },
-    { 'population': 1505005, 'coords': [48.1351, 11.5820], 'name': 'München' },
-    { 'population': 1024621, 'coords': [50.9375,  6.9603], 'name': 'Köln' },
-    { 'population':  756021, 'coords': [50.1109,  8.6821], 'name': 'Frankfurt' },
-    { 'population':  618685, 'coords': [51.2277,  6.7735], 'name': 'Düsseldorf' },
-    { 'population':  612663, 'coords': [48.7758,  9.1829], 'name': 'Stuttgart' },
-    { 'population':  611850, 'coords': [51.3397, 12.3731], 'name': 'Leipzig' },
-    { 'population':  603462, 'coords': [51.5136,  7.4653], 'name': 'Dortmund' },
-    { 'population':  574682, 'coords': [51.4556,  7.0116], 'name': 'Essen' },
-    { 'population':  569000, 'coords': [53.0736,  8.8064], 'name': 'Bremen' },
-    { 'population':  564904, 'coords': [51.0504, 13.7373], 'name': 'Dresden' },
-    { 'population':  526606, 'coords': [49.4521, 11.0767], 'name': 'Nürnberg' },
-    { 'population':  520290, 'coords': [52.3759,  9.7320], 'name': 'Hannover' },
-    { 'population':  502000, 'coords': [51.4325,  6.7652], 'name': 'Duisburg' },
-    { 'population':  366000, 'coords': [51.4818,  7.2162], 'name': 'Bochum' },
-    { 'population':  358193, 'coords': [51.2637,  7.2006], 'name': 'Wuppertal' },
-    { 'population':  340226, 'coords': [50.7374,  7.0982], 'name': 'Bonn' },
-    { 'population':  335000, 'coords': [52.0241,  8.5290], 'name': 'Bielefeld' },
-    { 'population':  321000, 'coords': [51.9624,  7.6257], 'name': 'Münster' },
-    { 'population':  315000, 'coords': [49.4875,  8.4660], 'name': 'Mannheim' },
-    { 'population':  310000, 'coords': [49.0069,  8.4037], 'name': 'Karlsruhe' },
-    { 'population':  300089, 'coords': [50.0375,  8.2660], 'name': 'Wiesbaden' },
-    { 'population':  298972, 'coords': [48.3772, 10.2522], 'name': 'Augsburg' },
-    { 'population':  268000, 'coords': [51.1854,  6.4417], 'name': 'Mönchengladbach' },
-    { 'population':  263000, 'coords': [51.5051,  7.0965], 'name': 'Gelsenkirchen' },
-    { 'population':  252000, 'coords': [52.2659, 10.5267], 'name': 'Braunschweig' },
-    { 'population':  252000, 'coords': [50.7756,  6.0836], 'name': 'Aachen' },
-    { 'population':  251699, 'coords': [50.8278, 12.9214], 'name': 'Chemnitz' },
-    { 'population':  249132, 'coords': [54.3233, 10.1228], 'name': 'Kiel' },
-    { 'population':  240114, 'coords': [52.1205, 11.6276], 'name': 'Magdeburg' },
-    { 'population':  236236, 'coords': [47.9990,  7.8421], 'name': 'Freiburg' },
-    { 'population':  227000, 'coords': [51.4833, 11.9667], 'name': 'Halle (Saale)' },
-    { 'population':  227000, 'coords': [51.3392,  6.5862], 'name': 'Krefeld' },
-    { 'population':  219549, 'coords': [50.9848, 11.0299], 'name': 'Erfurt' },
-    { 'population':  217272, 'coords': [49.9929,  8.2473], 'name': 'Mainz' },
-    { 'population':  217061, 'coords': [53.8655, 10.6866], 'name': 'Lübeck' },
-    { 'population':  210000, 'coords': [51.4781,  6.8625], 'name': 'Oberhausen' },
-    { 'population':  205307, 'coords': [54.0924, 12.0991], 'name': 'Rostock' },
-    { 'population':  201048, 'coords': [51.3127,  9.4797], 'name': 'Kassel' },
-    { 'population':  182971, 'coords': [49.2327,  6.9962], 'name': 'Saarbrücken' },
-    { 'population':  176110, 'coords': [49.4718,  8.4512], 'name': 'Ludwigshafen' },
-    { 'population':  155756, 'coords': [49.4103,  8.6971], 'name': 'Heidelberg' },
-    { 'population':  151389, 'coords': [49.0134, 12.1016], 'name': 'Regensburg' },
-    { 'population':  115298, 'coords': [50.3569,  7.5890], 'name': 'Koblenz' },
-    { 'population':  108056, 'coords': [50.9271, 11.5892], 'name': 'Jena' },
-    { 'population':  104342, 'coords': [49.7597,  6.6415], 'name': 'Trier' },
+    { 'population': 3755000, 'coords': [52.5200, 13.4050], 'state': 'DE-BE (Berlin)',                 'name': 'Berlin' },
+    { 'population': 1862565, 'coords': [53.5511,  9.9937], 'state': 'DE-HH (Hamburg)',                'name': 'Hamburg' },
+    { 'population': 1505005, 'coords': [48.1351, 11.5820], 'state': 'DE-BY (Bayern)',                 'name': 'München' },
+    { 'population': 1024621, 'coords': [50.9375,  6.9603], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Köln' },
+    { 'population':  756021, 'coords': [50.1109,  8.6821], 'state': 'DE-HE (Hessen)',                 'name': 'Frankfurt' },
+    { 'population':  618685, 'coords': [51.2277,  6.7735], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Düsseldorf' },
+    { 'population':  612663, 'coords': [48.7758,  9.1829], 'state': 'DE-BW (Baden-Württemberg)',      'name': 'Stuttgart' },
+    { 'population':  611850, 'coords': [51.3397, 12.3731], 'state': 'DE-SN (Sachsen)',                'name': 'Leipzig' },
+    { 'population':  603462, 'coords': [51.5136,  7.4653], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Dortmund' },
+    { 'population':  574682, 'coords': [51.4556,  7.0116], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Essen' },
+    { 'population':  569000, 'coords': [53.0736,  8.8064], 'state': 'DE-HH (Hamburg)',                'name': 'Bremen' },
+    { 'population':  564904, 'coords': [51.0504, 13.7373], 'state': 'DE-SN (Sachsen)',                'name': 'Dresden' },
+    { 'population':  526606, 'coords': [49.4521, 11.0767], 'state': 'DE-BY (Bayern)',                 'name': 'Nürnberg' },
+    { 'population':  520290, 'coords': [52.3759,  9.7320], 'state': 'DE-NI (Niedersachsen)',          'name': 'Hannover' },
+    { 'population':  502000, 'coords': [51.4325,  6.7652], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Duisburg' },
+    { 'population':  366000, 'coords': [51.4818,  7.2162], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Bochum' },
+    { 'population':  358193, 'coords': [51.2637,  7.2006], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Wuppertal' },
+    { 'population':  340226, 'coords': [50.7374,  7.0982], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Bonn' },
+    { 'population':  335000, 'coords': [52.0241,  8.5290], 'state': 'DE-NI (Niedersachsen)',          'name': 'Bielefeld' },
+    { 'population':  321000, 'coords': [51.9624,  7.6257], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Münster' },
+    { 'population':  315000, 'coords': [49.4875,  8.4660], 'state': 'DE-BW (Baden-Württemberg)',      'name': 'Mannheim' },
+    { 'population':  310000, 'coords': [49.0069,  8.4037], 'state': 'DE-BW (Baden-Württemberg)',      'name': 'Karlsruhe' },
+    { 'population':  300089, 'coords': [50.0375,  8.2660], 'state': 'DE-HE (Hessen)',                 'name': 'Wiesbaden' },
+    { 'population':  298972, 'coords': [48.3772, 10.2522], 'state': 'DE-BY (Bayern)',                 'name': 'Augsburg' },
+    { 'population':  268000, 'coords': [51.1854,  6.4417], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Mönchengladbach' },
+    { 'population':  263000, 'coords': [51.5051,  7.0965], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Gelsenkirchen' },
+    { 'population':  252000, 'coords': [52.2659, 10.5267], 'state': 'DE-NI (Niedersachsen)',          'name': 'Braunschweig' },
+    { 'population':  252000, 'coords': [50.7756,  6.0836], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Aachen' },
+    { 'population':  251699, 'coords': [50.8278, 12.9214], 'state': 'DE-SN (Sachsen)',                'name': 'Chemnitz' },
+    { 'population':  249132, 'coords': [54.3233, 10.1228], 'state': 'DE-HH (Hamburg)',                'name': 'Kiel' },
+    { 'population':  240114, 'coords': [52.1205, 11.6276], 'state': 'DE-SN (Sachsen)',                'name': 'Magdeburg' },
+    { 'population':  236236, 'coords': [47.9990,  7.8421], 'state': 'DE-BW (Baden-Württemberg)',      'name': 'Freiburg' },
+    { 'population':  227000, 'coords': [51.4833, 11.9667], 'state': 'DE-NI (Niedersachsen)',          'name': 'Halle (Saale)' },
+    { 'population':  227000, 'coords': [51.3392,  6.5862], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Krefeld' },
+    { 'population':  219549, 'coords': [50.9848, 11.0299], 'state': 'DE-SN (Sachsen)',                'name': 'Erfurt' },
+    { 'population':  217272, 'coords': [49.9929,  8.2473], 'state': 'DE-BY (Bayern)',                 'name': 'Mainz' },
+    { 'population':  217061, 'coords': [53.8655, 10.6866], 'state': 'DE-HH (Hamburg)',                'name': 'Lübeck' },
+    { 'population':  210000, 'coords': [51.4781,  6.8625], 'state': 'DE-NW (Nordrhein-Westfalen)',    'name': 'Oberhausen' },
+    { 'population':  205307, 'coords': [54.0924, 12.0991], 'state': 'DE-MV (Mecklenburg-Vorpommern)', 'name': 'Rostock' },
+    { 'population':  201048, 'coords': [51.3127,  9.4797], 'state': 'DE-NI (Niedersachsen)',          'name': 'Kassel' },
+    { 'population':  182971, 'coords': [49.2327,  6.9962], 'state': 'DE-BW (Baden-Württemberg)',      'name': 'Saarbrücken' },
+    { 'population':  176110, 'coords': [49.4718,  8.4512], 'state': 'DE-BW (Baden-Württemberg)',      'name': 'Ludwigshafen' },
+    { 'population':  155756, 'coords': [49.4103,  8.6971], 'state': 'DE-BW (Baden-Württemberg)',      'name': 'Heidelberg' },
+    { 'population':  151389, 'coords': [49.0134, 12.1016], 'state': 'DE-SN (Sachsen)',                'name': 'Regensburg' },
+    { 'population':  115298, 'coords': [50.3569,  7.5890], 'state': 'DE-RP (Rheinland-Pfalz)',        'name': 'Koblenz' },
+    { 'population':  108056, 'coords': [50.9271, 11.5892], 'state': 'DE-TH (Thüringen)',              'name': 'Jena' },
+    { 'population':  104342, 'coords': [49.7597,  6.6415], 'state': 'DE-RP (Rheinland-Pfalz)',        'name': 'Trier' },
 ]
 
-def find_nearest_city(lat: float, lon: float) -> tuple[dict, float] | None:
+def find_nearest_city(lat: float, lon: float) -> tuple[dict | None, float]:
     nearest_city = None
     nearest_dist = float('inf')
 
@@ -133,7 +133,8 @@ def find_nearest_city(lat: float, lon: float) -> tuple[dict, float] | None:
 
     if nearest_city:
         return (nearest_city, nearest_dist)
-    return None
+    else:
+        return (None, 0.0)
 
 
 @disk_cache.cache(VERSION)
@@ -253,19 +254,14 @@ def main(argv: list[str] = sys.argv[1:]) -> int:
         if not (lat and lon):
             continue
 
-        nearest_result = find_nearest_city(lat, lon)
-        if nearest_result:
-            nearest, city_dist = nearest_result
-            city_name = nearest['name']
-        else:
-            city_name = "Unknown"
-            city_dist = 0.0
+        nearest, city_dist = find_nearest_city(lat, lon)
 
         try:
             event_items.append({
                 "name": termin.get("name", termin.get("ort", "Unknown")),
                 "plz": termin["plz"],
-                "city": city_name,
+                "state": nearest.get("state", "Unknown"),
+                "city": nearest.get("name", "Unknown"),
                 "city_dist": round(city_dist, 1),
                 "coords": [lat, lon],
                 "date": termin['beginn'].split(" ")[0],
